8dfc2c596149ff422ad64c1a85013d8e635f92b2
Harjun751 <Harjun751@gmail.com>
2026-09-25 08:54:14 +0800

    Remove isActive field from user.entity
    isActive is not being used and is achieved
    through the isLocked field.

--- user-service/src/users/users.service.ts
+++ user-service/src/users/users.service.ts
@@ -54,7 +54,6 @@ export class UsersService {
           displayName,
           passwordHash,
           passwordSalt,
-          isActive: true,
           isArchived: false,
           isAdmin,
           isLocked,
